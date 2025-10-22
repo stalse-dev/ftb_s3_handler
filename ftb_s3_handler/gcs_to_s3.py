06e5bd9fbec33a36debd7959be3fe59148c365f6
Art0rStalse <arthur.fernandes@stalse.com>
2025-10-22 11:47:47 -0300

    feat: dockerfile for gcs_to_s3 and custom key by environment

--- ftb_s3_handler/gcs_to_s3.py
+++ ftb_s3_handler/gcs_to_s3.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     gcs_client = GCSClient().get_gcs_client()
     s3_client = S3Client().get_s3_client()
     bucket_id = os.environ["BUCKET_ID"]
+    s3_landing = os.environ["S3_LANDING"]
 
     bucket: Bucket = gcs_client.bucket(bucket_id)
     blobs = bucket.list_blobs()
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
         year = blob.name.split("_")[1]
         content = blob.download_as_bytes()
-        key = "api-project-1033684201634/analytics_153835980/events/{0}/12/31/{1}".format(year, blob.name)
+        key = s3_landing.format(year, blob.name)
 
         s3_client.put_object(
             Bucket="gcp-bigquery-stalse-landing",
